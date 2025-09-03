a5fe00c811ba6d89924f3290e1617dec3c727672
Graeyson <graeyson.chadwick@gmail.com>
2025-09-03 21:53:55 +1200

    Changes to Work
    Did some debugging to allow the code to run on my browser.

---
+++ music_dayone.js
@@ -0,0 +1,19 @@
+let sunY = 350;
+let sunSize;
+
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+    background(20)
+    sunSize = map(drum, 0, 100, 50, 400);
+
+    fill(255, 255, 0); //yellow
+
+    ellipse(300, sunY, sunSize);
+    
+    
+    if(sunY > 1000){
+        sunY=0
+    }
+
+    
+}
